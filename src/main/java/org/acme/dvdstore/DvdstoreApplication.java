688982c808f8963371b45684e32a77ec7ea903dd
Thodoris Dimaras <t.dimaras@outlook.com>
2018-12-18 10:13:13 +0200

    Initial commit

---
+++ src/main/java/org/acme/dvdstore/DvdstoreApplication.java
@@ -0,0 +1,14 @@
+package org.acme.dvdstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdstoreApplication.class, args);
+	}
+
+}
+
